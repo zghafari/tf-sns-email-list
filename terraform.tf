be04b8885f4d955ad7e28e73b0dac430a649199a
asad-awadia <asad.awadia@gmail.com>
2021-08-24 17:04:21 -0400

    update tf to 14

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,8 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.14"
 
   required_providers {
-    aws      = "~> 2.19"
+    aws      = "~> 3.0"
     template = "~> 2.1"
   }
 }
